8748459beac4ed936b3a457c26df525e9449f88c
Evgeniy Rolenko <jeka@rolenko.ru>
2022-05-03 23:06:06 +0300

    Capitalize done

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -8,6 +8,14 @@
 
 function capitalize(str) {
     // Напишите код здесь
+    let arr = str.split(' ');
+    let newArr = [];
+    let newStr;
+    arr.forEach((item) => {
+        let newWord = item.slice(0,1).toUpperCase() + item.slice(1);
+        newArr.push(newWord);
+    })
+    return newStr = newArr.join(' ');
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
