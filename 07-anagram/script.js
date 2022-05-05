773323a1e36b3b7466a05d26551c56d3e7291660
Evgeniy Rolenko <jeka@rolenko.ru>
2022-05-05 23:00:53 +0300

    anagram done

--- 07-anagram/script.js
+++ 07-anagram/script.js
@@ -10,9 +10,16 @@
 
 function anagram(str1, str2) {
     // Напишите код здесь
+    const sort1 = str1.toLowerCase().split("").sort().join("");
+    const sort2 = str2.toLowerCase().split("").sort().join("");
+
+    return sort1 === sort2;
+
+
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(anagram('finder', 'Friend')); // true
 console.log(anagram('hello', 'bye')); // false
+console.log(anagram('up', 'UP'));
