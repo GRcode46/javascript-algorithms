4664075161a818b82d6d4b79bd71eac3f0dec77b
Evgeniy Rolenko <jeka@rolenko.ru>
2022-05-06 00:15:53 +0300

    fibonacci done

--- 08-fibonacci/script.js
+++ 08-fibonacci/script.js
@@ -9,9 +9,29 @@
 */
 
 function fibonacci(n) {
-    // Напишите код здесь
+    if (n <=1 ){
+        return 0;
+    } else if (n <= 3) {
+        return 1;
+    } else  {
+        let fibInt1 = 1;
+        let fibInt2 = 1;
+        for (let i = 4; i <= n; i++) {
+            let fibInt3 = fibInt1 + fibInt2;
+            fibInt1 = fibInt2;
+            fibInt2 = fibInt3;
+        }
+        return fibInt2;
+    }
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(fibonacci(4)); // 2. Четвёртое число последовательности — двойка (0, 1, 1, 2)
+// console.log(fibonacci(4)); // 2. Четвёртое число последовательности — двойка (0, 1, 1, 2)
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
